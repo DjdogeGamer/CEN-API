4099bba105591a83f935a44a29a7ce8e8e04237c
Gustavo <ghco@cesar.school>
2023-05-05 15:56:07 +0100

    Initial Commit

---
+++ APITEST.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+const ENDPOINT = 'http://localhost:3000';
+
+// Query endpoint
+axios.get(ENDPOINT, {
+    params: {
+        sqlQuery: 'SELECT * FROM tabela_teste',
+        fileName: 'db.sqlite'
+    }
+}).then((response) => {
+    console.log(response.data);
+}).catch((error) => {
+    console.error(error);
+});
+
+// Asset endpoint
+axios.get(ENDPOINT, {
+    params: {
+        fileName: 'menu_bg.jpg'
+    }
+}).then((response) => {
+    console.log(response.data);
+}).catch((error) => {
+    console.error(error);
+});
---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const S3UtilAPI = require('./S3UtilAPI');
+
+const app = express();
+
+const bucketName = 'sqlite-js-ispg-test';
+const fileName = 'db.sqlite';
+const accessKeyId = 'AKIA4ZDOYH5F6H3ASIGQ';
+const secretAccessKey = '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';
+
+const S3Util = new S3UtilAPI(bucketName, accessKeyId, secretAccessKey);
+
+app.get('/', (req, res) => {
+    res.send('CEN-API is running.');
+} );
+
+app.get('/query', (req, res) => {
+    const sqlQuery = req.query.sqlQuery;
+    const fileName = req.query.fileName;
+    S3Util.executeQuery(sqlQuery, fileName, (result) => {
+        res.send(result);
+    });
+});
+
+app.get('/asset', (req, res) => {
+    const fileName = req.query.fileName;
+    S3Util.loadAsset(fileName, (result) => {
+        res.send(result);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000.');
+});
